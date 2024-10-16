a6613e11657032d36f04a62a819dc0f1005bec1b
Connor Gannaway <charlesconnorgannaway@gmail.com>
2024-10-16 10:51:48 -0400

    es6 + styled components cause its easier to read + cookie clicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CookieClicker from "./views/cookieclicker";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <CookieClicker />
     </div>
   );
 }
