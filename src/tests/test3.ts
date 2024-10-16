aa01b0a8b5b4d349ffc971942e850fd3095b8fc8
CalebKornegay <caleb.kornegay@gmail.com>
2024-10-16 16:44:49 -0400

    Fixing CORS policy issues"

--- src/tests/test3.ts
+++ src/tests/test3.ts
@@ -1,3 +1,8 @@
+import getCurrentTabURL from "../hooks/getCurrentTabURL";
+
 export default function test3() {
+    getCurrentTabURL().then(url => {
+        fetch(url).then(console.log)
+    });
     return true;
 }
