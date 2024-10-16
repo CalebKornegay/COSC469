ec4b02aec1844338197f66c9e71d68c0f115da34
CalebKornegay <caleb.kornegay@gmail.com>
2024-10-16 18:25:13 -0400

    Adding DNS record checks using dns.google.com

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,5 +1,5 @@
 export { default as test1 } from "./test1";
 export { default as test2 } from "./test2";
-export { default as test3 } from "./test3";
-export { default as URLCheck } from "./URLCheck";
-export { default as SpellCheck } from "./SpellCheck";
+export { default as dnsCheck } from "./dnsCheck";
+// export { default as URLCheck } from "./URLCheck";
+// export { default as SpellCheck } from "./SpellCheck";
