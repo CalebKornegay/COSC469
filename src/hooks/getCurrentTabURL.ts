409359703944b99ccf2ff047b58570e108a23a16
Connor Gannaway <charlesconnorgannaway@gmail.com>
2024-10-16 16:28:12 -0400

    fix

--- src/hooks/getCurrentTabURL.ts
+++ src/hooks/getCurrentTabURL.ts
@@ -1,10 +1,10 @@
-export default function getCurrentTabURL(): Promise<string | undefined> {
+export default function getCurrentTabURL(): Promise<string> {
   return new Promise((resolve, reject) => {
     chrome.tabs.query(
       { active: true, windowId: chrome.windows.WINDOW_ID_CURRENT },
       function (tabs) {
         if (tabs.length > 0) {
-          resolve(tabs[0].url);
+          resolve(tabs[0].url || "");
         } else {
           reject("No active tab found");
         }
--- src/tests/CheckAgainstDatabase.ts
+++ src/tests/CheckAgainstDatabase.ts
@@ -1,5 +1,11 @@
-export default function CheckAgainstDatabase() {
+import getCurrentTabURL from "../hooks/getCurrentTabURL";
+
+export default async function CheckAgainstDatabase() {
   const baseurl = "https://phish.gannaway.co/check/";
+
+  const currenturl = await getCurrentTabURL();
+
+  let httpurl = currenturl.replace("https", "http");
 
   return false;
 }
