d9a5114898c300dafc8c067268d2798a97a52a6a
Connor Gannaway <charlesconnorgannaway@gmail.com>
2024-10-16 14:44:50 -0400

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Settings, { SettingsContext } from './settings';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Settings, { SettingsContext } from "./settings";
+import Tests from "./views/tests";
 
 export default function App() {
-    const user_settings = React.useContext(SettingsContext);
-    return (
-        <div className="App">
-            <Settings />
-            <p>Hello World</p>
-        </div>
-    );
+  const user_settings = React.useContext(SettingsContext);
+  return (
+    <div className="App">
+      <Settings />
+      <div className="AppHeader">
+        <p>APeBEhavior</p>
+      </div>
+
+      <Tests />
+    </div>
+  );
 }
