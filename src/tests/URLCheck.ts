6e9c8eeaaa5e0e2b79aba3cf6277e0db00fbc308
aaronpig9 <aaronking799@gmail.com>
2024-10-16 18:29:16 -0400

    Got ChatGPT query to work. Commented out URLCheck so it doesn't query GPT every time we test other functions.

--- src/tests/URLCheck.ts
+++ src/tests/URLCheck.ts
@@ -2,23 +2,17 @@ import getCurrentTabURL from "../hooks/getCurrentTabURL";
 import GPTQuery from "../hooks/GPTQuery";
 
 export default async function URLCheck() {
-    const currenturl = await getCurrentTabURL();
+    // const currenturl = await getCurrentTabURL();
 
-    console.log("Does this site look like a phishing URL? Answer 'yes' or 'no' only please: " + currenturl);
+    // const response = await GPTQuery("Does this site look like a phishing URL? Answer 'Yes' or 'No' only please: " + currenturl);
 
-    GPTQuery("Does this site look like a phishing URL? Answer 'Yes' or 'No' only please: " + currenturl)
-        .then(response => {
-            console.log("ChatGPT Response:", response);  // The response will be a string with ChatGPT's answer
-            if(response == "Yes") {
-                return true;
-            }
-            else {
-                return false;
-            };
-        })
-        .catch(error => {
-            console.error("Error:", error);
-    });
+    // if (response == "No") {
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // }
 
-  return false;
+    return false;
+
 }
