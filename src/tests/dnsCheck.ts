e252d68bd305578b31ab84970b00c3403755a63e
CalebKornegay <caleb.kornegay@gmail.com>
2024-10-16 18:51:44 -0400

    Adding DNS record checks using dns.google.com

--- src/tests/dnsCheck.ts
+++ src/tests/dnsCheck.ts
@@ -1,17 +1,11 @@
 import getCurrentTabURL from "../hooks/getCurrentTabURL";
 
 export default async function dnsCheck() {
-    getCurrentTabURL().then(url => {
-        console.log(url);
-        let query_url = url.replace('https://', '').replace('http://', '').replace('www.', '');
-        query_url = query_url.substring(0, query_url.indexOf('/'));
-        fetch('https://dns.google.com/resolve?name=' + query_url + '&rr_type=ALL').then(resp => {
-            return resp.json();
-        }).then((json: any) => {
-            json?.Status === 0 ? console.log('Good domain') : console.log('Bad domain');
-        });
-        fetch(url).then(console.log);
-        console.log(query_url);
-    });
-    return true;
+    const url = await getCurrentTabURL();
+    let query_url = url.replace('https://', '').replace('http://', '').replace('www.', '');
+    query_url = query_url.substring(0, query_url.indexOf('/'));
+    
+    const resp = await fetch('https://dns.google.com/resolve?name=' + query_url + '&rr_type=ALL')
+    const json: any = await resp.json()
+    return json?.Status === 0 ? true : false;
 }
