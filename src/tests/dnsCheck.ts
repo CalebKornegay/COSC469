340801d7d3adf1502a3ee5a76ee9ccc540302e1f
CalebKornegay <caleb.kornegay@gmail.com>
2024-10-23 16:11:41 -0400

    Adding some more granularity to DNS

--- src/tests/dnsCheck.ts
+++ src/tests/dnsCheck.ts
@@ -5,7 +5,26 @@ export default async function dnsCheck() {
     let query_url = url.replace('https://', '').replace('http://', '').replace('www.', '');
     query_url = query_url.substring(0, query_url.indexOf('/'));
     
-    const resp = await fetch('https://dns.google.com/resolve?name=' + query_url + '&rr_type=ALL')
-    const json: any = await resp.json()
-    return json?.Status === 0 ? true : false;
+    const resp = await fetch('https://dns.google.com/resolve?name=' + query_url + '&rr_type=A');
+    const json: any = await resp.json();
+
+    let ttl: number | undefined = undefined;
+    for (let i = 0; i < json?.Answer?.length; ++i) {
+        ttl = json?.Answer?.[i]?.TTL;
+        if (ttl && ttl > 3000) {
+            console.log('ttl: ' + ttl);
+            return false;
+        }
+    }
+
+    for (let i = 0; i < json?.Authority?.length; ++i) {
+        ttl = json?.Authority?.[i]?.TTL;
+        if (ttl && ttl > 3000) {
+            console.log('ttl: ' + ttl);
+            return false;
+        }
+    }
+
+    console.log('A record status: ' + (json?.Status === 0));
+    return json?.Status === 0;
 }
