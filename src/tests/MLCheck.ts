8861bc43afd09faa776eded17b54d94419173eff
Connor Gannaway <charlesconnorgannaway@gmail.com>
2024-10-24 14:17:25 -0400

    hook up ml check to endpoint

--- src/tests/MLCheck.ts
+++ src/tests/MLCheck.ts
@@ -1,8 +1,12 @@
 import getCurrentTabURL from "../hooks/getCurrentTabURL";
 
 export default async function MLCheck() {
+    const baseurl = "https://phish.gannaway.co/ml/?url=";
     const currenturl = await getCurrentTabURL();
-
-
-    return false;
+    const strippedurl = currenturl.replace(/https?:\/\//, "");
+    const response = await fetch(baseurl + strippedurl);
+    const data = await response.json();
+    if (data["error"]) return true;
+    if (data["isPhishing"] >= 0.80) return false;
+    return true;
 }
