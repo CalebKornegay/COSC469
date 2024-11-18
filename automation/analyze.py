23731766f71b0b9532896ab11cda910af95611a6
Caleb Kornegay <caleb.kornegay@gmail.com>
2024-11-18 16:15:17 -0500

    Adding confusion matrices

--- automation/analyze.py
+++ automation/analyze.py
@@ -15,35 +15,54 @@ f_pos: list[int] = [0] * len(tests)
 t_neg: list[int] = [0] * len(tests)
 f_neg: list[int] = [0] * len(tests)
 num_correct: int = 0
+num_pred_phishing: int = 0
 non_phishing: int = 0
 phishing: int = 0
 conf_matrices: list = []
-y_real: list[list] = []
-y_act: list[list] = []
+y_real: list[list[bool]] = []
+y_act: list[list[bool]] = []
+overall_real: list[bool] = []
+overall_act: list[bool] = []
+i = 0
 
 for index, result in enumerate(test_results):
     result_status: dict = eval(result)
     non_phishing += result_status["isPhishing"] == False
     phishing += result_status["isPhishing"] == True
     # if non_phishing == phishing: break
+
     correct: bool = True
+    num_phishing: int = 0
+    pred_phishing: bool = False
     real = []
     act = []
 
     for test_index, test in enumerate(tests):
-        t_pos[test_index] += result_status[test] == True and result_status["isPhishing"] == True
-        f_pos[test_index] += result_status[test] == True and result_status["isPhishing"] == False
-        t_neg[test_index] += result_status[test] == False and result_status["isPhishing"] == False
-        f_neg[test_index] += result_status[test] == False and result_status["isPhishing"] == True
-        real.append(result_status["isPhishing"])
-        act.append(result_status[test])
+        t_pos[test_index] += result_status[test] == False and result_status["isPhishing"] == True
+        f_pos[test_index] += result_status[test] == False and result_status["isPhishing"] == False
+        t_neg[test_index] += result_status[test] == True and result_status["isPhishing"] == False
+        f_neg[test_index] += result_status[test] == True and result_status["isPhishing"] == True
 
-        if result_status[test] != result_status["isPhishing"]: correct = False
+        if not result_status[test]: pred_phishing = True
+        num_phishing += not result_status[test]
+        
+        act.append(result_status["isPhishing"])
+        real.append(not result_status[test])
+
+        if result_status[test] == result_status["isPhishing"]: correct = False
+
+    overall_act.append(result_status["isPhishing"])
+    if num_phishing == 5 and result_status["isPhishing"] or num_phishing == 0 and not result_status["isPhishing"]:
+        overall_real.append(result_status["isPhishing"])
+    else:
+        overall_real.append(not result_status["isPhishing"])
+    # overall_real.append(pred_phishing)
 
     y_real.append(real)
     y_act.append(act)
 
     num_correct += correct
+    # num_correct += (pred_phishing == result_status["isPhishing"])
 
 for test_index, test in enumerate(tests):
     print(f"True positive rate for {test} test = {t_pos[test_index] / (t_pos[test_index] + f_neg[test_index]) * 100}%")
@@ -57,8 +76,7 @@ print(f"Overall testing accuracy is {num_correct / (phishing + non_phishing) * 1
 y_real = np.array(y_real).T
 y_act = np.array(y_act).T
 
-conf_matrices = [ConfusionMatrixDisplay(confusion_matrix=np.array(confusion_matrix(real, act))) for (real, act) in zip(y_real, y_act)]
-
+conf_matrices = [ConfusionMatrixDisplay(confusion_matrix=np.array(confusion_matrix(act, real))) for (act, real) in zip(y_act, y_real)] + [ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(overall_act, overall_real))]
 
 for i, matrix in enumerate(conf_matrices):
     matrix.plot()
